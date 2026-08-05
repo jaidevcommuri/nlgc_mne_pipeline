920d7b644d7ef8965772d923ac9eb2433de68a00
Jaidev Commuri <jcommuri@mac.myfiosgateway.com>
2026-08-04 21:27:38 -0400

    adjusted nlgc_map parameters to account for the new build of NLGC (refactor). Additionally changed the code to not use eelfarm since it does not function properly with matplotlib

--- testline.py
+++ testline.py
@@ -3,6 +3,7 @@ import pathlib
 import eelfarm
 import nlgc
 import numpy
+import eelbrain as eel
 
 root = pathlib.Path(f"/Volumes/PortableSSD/Alzheimers")
 megdir = root / 'meg_dir'
@@ -13,13 +14,19 @@ result_dir = root / 'results'
 # meg data was derived from KIT system
 def preprocess(sub, session, start=0, duration=60, return_vars=True, resting=False, empty=False, ico1=False, ico4=False, fwd=False, covariance=False):
     
+    resting_path = pathlib.Path(f"{megdir}/{sub}/{sub}_tsss-1-45-causal-resting_{session}-ica-apply.fif")
+    empty_path = pathlib.Path(f"{megdir}/{sub}/{sub}_tsss-emptyroom.fif")
+    ico1_path = pathlib.Path(f"{mridir}/{sub}/bem/{sub}_ico-1-src.fif")
+    ico4_path = pathlib.Path(f"{mridir}/{sub}/bem/{sub}_ico-4-src.fif")
+    evoked_path = pathlib.Path(f"{megdir}/{sub}/{sub}_{session}_evoked-ave.fif")
+    forward_path = pathlib.Path(f"{mridir}/{sub}/bem/{sub}_{session}-solution-fwd.fif")
     trans_path = pathlib.Path(f"{megdir}/{sub}/{sub}-trans.fif")
     bem_path = pathlib.Path(f"{mridir}/{sub}/bem/{sub}-inner_skull-bem-sol.fif")
+    cov_path = pathlib.Path(f"{megdir}/{sub}/{sub}_cov.fif")
 
     assert trans_path.exists()
     assert bem_path.exists()
 
-    resting_path = pathlib.Path(f"{megdir}/{sub}/{sub}_tsss-1-45-causal-resting_{session}-ica-apply.fif")
     if (resting==True or resting_path.exists()==False):
         raw = mne.io.read_raw_fif(f"{megdir}/{sub}/{sub}_resting_{session}-raw.fif", preload=True)
         raw = raw.pick(picks='meg', exclude='bads')
@@ -39,7 +46,6 @@ def preprocess(sub, session, start=0, duration=60, return_vars=True, resting=Fal
     else:
         ica_apply = mne.io.read_raw_fif(fname=resting_path, preload=True)
 
-    empty_path = pathlib.Path(f"{megdir}/{sub}/{sub}_tsss-emptyroom.fif")
     if (empty==True or empty_path.exists()==False):
         raw_empty = mne.io.read_raw_fif(f"{megdir}/{sub}/{sub}_emptyroom-raw.fif", preload=True)
         raw_empty = raw_empty.pick(picks='meg', exclude='bads')
@@ -51,7 +57,6 @@ def preprocess(sub, session, start=0, duration=60, return_vars=True, resting=Fal
     else:
         empty_causal = mne.io.read_raw_fif(empty_path, preload=True)
     
-    ico1_path = pathlib.Path(f"{mridir}/{sub}/bem/{sub}_ico-1-src.fif")
     if (ico1==True or ico1_path.exists()==False):
         # white and pial both possible surfaces. pial may be better
         # add dist may or may not affect final results. More testing is needed
@@ -60,7 +65,6 @@ def preprocess(sub, session, start=0, duration=60, return_vars=True, resting=Fal
     else:
         src1 = mne.read_source_spaces(fname=ico1_path)
 
-    ico4_path = pathlib.Path(f"{mridir}/{sub}/bem/{sub}_ico-4-src.fif")
     if (ico4==True or ico4_path.exists()==False):
         # white and pial both possible surfaces. pial may be better
         # add dist may or may not affect final results. More testing is needed
@@ -70,7 +74,6 @@ def preprocess(sub, session, start=0, duration=60, return_vars=True, resting=Fal
         src4 = mne.read_source_spaces(fname=ico4_path)
     
     ica_apply = ica_apply.crop(tmin=start, tmax=start+duration)
-    evoked_path = pathlib.Path(f"{megdir}/{sub}/{sub}_{session}_evoked-ave.fif")
     epochs = mne.make_fixed_length_epochs(ica_apply, duration=duration, reject_by_annotation=False, preload=True, verbose=True)
     print(epochs)
     print(type(epochs))
@@ -83,7 +86,6 @@ def preprocess(sub, session, start=0, duration=60, return_vars=True, resting=Fal
     
     trans = mne.read_trans(fname=trans_path)
     bem = mne.read_bem_solution(fname=bem_path)
-    forward_path = pathlib.Path(f"{mridir}/{sub}/bem/{sub}_{session}-solution-fwd.fif")
     if (fwd==True or forward_path.exists()==False):
         # cv=3, conflict with nlgc's cv=5?
         forward = mne.make_forward_solution(evoked_data.info, trans=trans, src=src4, bem=bem, meg=True, eeg=False, ignore_ref=True, verbose=True)
@@ -94,7 +96,7 @@ def preprocess(sub, session, start=0, duration=60, return_vars=True, resting=Fal
     forward = mne.convert_forward_solution(forward, force_fixed=True)
 
     
-    cov_path = pathlib.Path(f"{megdir}/{sub}/{sub}_cov.fif")
+    
     if (covariance==True or cov_path.exists()==False):
         # check method
         empty_causal = empty_causal.crop(tmin=3, tmax=empty.times[-1] - 3)
@@ -107,29 +109,36 @@ def preprocess(sub, session, start=0, duration=60, return_vars=True, resting=Fal
         return evoked_data, forward, cov, src1
     return
 
-
-
-
-# tm21 - init
-# tm22 - ico1 made with surface=pial
-
 server = eelfarm.start_server('localhost')
 subject = 'A201'
-sessions = ['EO1', 'EC1']
+sessions = ['EC1', 'EO1']
 for session in sessions:
-    lam = [0.1]
+    lam = [0.05]
     for trial in range(3):
         try:
             duration = 58
             start_time = 3 + trial * duration
             # evoked, fwd, cov, src = preprocess(sub=subject, session=session, resting=True, empty=True, evoked=True, ico1=True, ico4=True, fwd=True, covariance=True, return_vars=True)
+            # if (start_time == 3):
+            #     continue
             evoked, fwd, cov, src = preprocess(sub=subject, session=session, start=start_time, duration=duration, return_vars=True)
-            dst = f"{result_dir}/beta25hz/4eig2ord/[{subject}]-[visit=]-[session={session}]-[trial={trial}]-[beta]-[testmodel55].p"
+
+            dst = f"{result_dir}/beta25hz/4eig2ord/[{subject}]-[visit=]-[session={session}]-[trial={trial}]-[beta]-[testmodel82].p"
             print("nlgc start")
-            server.put(dst, nlgc.nlgc_map, subject, evoked, fwd, cov, src, use_es=False,
-                order=2, n_eigenmodes=1, patch_idx=list(range(84)),
+            # server.put(dst, nlgc.nlgc_map, subject, evoked, fwd, cov, src, use_es=False,
+            #     order=2, n_eigenmodes=2, patch_idx=list(range(84)),
+            #     lambda_range=lam, max_iter=500, max_cyclic_iter=3,
+            #     tol=1e-5, sparsity_factor=0.0, var_thr=1.0, cv=5, 
+            #     n_orients=1, parallel_process='multiprocess', verbose=True)
+            obj = nlgc.nlgc_map(subject, evoked, fwd, cov, src, use_es=False,
+                order=2, n_eigenmodes=4, patch_idx=list(range(84)),
                 lambda_range=lam, max_iter=500, max_cyclic_iter=3,
-                tol=1e-5, sparsity_factor=0.0, var_thr=1.0, cv=5)
+                tol=1e-5, sparsity_factor=0.0, var_thr=1.0, cv=5, 
+                n_orients=1, parallel_mode='multiprocess', verbose=True)
+
+            eel.save.pickle(obj, dst)
+
+
         except Exception as e:
             print(subject, session, trial, " encountered an error ", e)
             continue
